cb6d572f98364bc3ae7f239169fb3fc2e86ba4bc
Demi M. Obenour <demiobenour@gmail.com>
2018-12-26 16:30:22 -0500

    Fix test suite
    Truffle 5.0 is not backwards-compatible with earlier versions, requiring
    many changes.

--- truffle.js
+++ truffle.js
@@ -18,6 +18,13 @@ module.exports = {
       version: "^0.4.24",
     },
   },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+  },
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
